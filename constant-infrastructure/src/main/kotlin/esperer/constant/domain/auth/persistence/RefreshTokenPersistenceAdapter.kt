7df346ea327e0240482cbd79f52307f136e358ac
aesperer <piyrw9754@gmail.com>
2023-06-05 14:29:38 +0900

    create :: refresh token persistence adapter

--- constant-infrastructure/src/main/kotlin/esperer/constant/domain/auth/persistence/RefreshTokenPersistenceAdapter.kt
+++ constant-infrastructure/src/main/kotlin/esperer/constant/domain/auth/persistence/RefreshTokenPersistenceAdapter.kt
@@ -1,20 +1,21 @@
 package esperer.constant.domain.auth.persistence
 
+import esperer.constant.domain.auth.mapper.RefreshTokenMapper
 import esperer.constant.domain.auth.model.RefreshToken
 import esperer.constant.domain.auth.persistence.repository.RefreshTokenRepository
 import esperer.constant.domain.auth.spi.RefreshTokenPort
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 
 @Component
 class RefreshTokenPersistenceAdapter(
-    private val refreshTokenRepository: RefreshTokenRepository
+    private val refreshTokenRepository: RefreshTokenRepository,
+    private val refreshTokenMapper: RefreshTokenMapper
 ) : RefreshTokenPort {
 
-    override fun queryRefreshTokenByToken(token: String): RefreshToken? {
-        TODO("Not yet implemented")
-    }
+    override fun queryRefreshTokenByToken(token: String): RefreshToken? =
+        refreshTokenMapper.toDomain(refreshTokenRepository.findByIdOrNull(token))
 
-    override fun save(refreshToken: RefreshToken): RefreshToken {
-        TODO("Not yet implemented")
-    }
+    override fun save(refreshToken: RefreshToken): RefreshToken =
+        refreshTokenMapper.toDomain(refreshTokenRepository.save(refreshTokenMapper.toEntity(refreshToken)))!!
 }
