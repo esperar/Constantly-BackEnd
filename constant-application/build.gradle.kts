5f1a7ef89b788d60424dbad38ebb848a76451d99
aesperer <piyrw9754@gmail.com>
2023-06-10 12:35:25 +0900

    update :: all open usecase annotation

--- constant-application/build.gradle.kts
+++ constant-application/build.gradle.kts
@@ -1,2 +1,8 @@
 plugins {
+    kotlin("plugin.allopen") version PluginVersions.ALLOPEN_VERSION
+}
+
+allOpen {
+    annotation("esperer.common.annotation.UseCase")
+    annotation("esperer.common.annotation.ReadOnlyUseCase")
 }
