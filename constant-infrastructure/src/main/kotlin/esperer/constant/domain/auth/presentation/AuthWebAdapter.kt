791ddb3f483c082defaf9872bd55b32d699931b7
aesperer <piyrw9754@gmail.com>
2023-06-10 12:23:08 +0900

    create :: auth presentation

---
+++ constant-infrastructure/src/main/kotlin/esperer/constant/domain/auth/presentation/AuthWebAdapter.kt
@@ -0,0 +1,11 @@
+package esperer.constant.domain.auth.presentation
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/auth")
+class AuthWebAdapter {
+
+
+}
