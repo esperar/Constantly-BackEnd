3c018f6bd4c891069483e2c454be621db0dcbe5c
aesperer <piyrw9754@gmail.com>
2023-06-10 14:44:44 +0900

    create :: mapper component

--- constant-infrastructure/src/main/kotlin/esperer/constant/domain/sprint/mapper/SprintMapper.kt
+++ constant-infrastructure/src/main/kotlin/esperer/constant/domain/sprint/mapper/SprintMapper.kt
@@ -3,7 +3,9 @@ package esperer.constant.domain.sprint.mapper
 import esperer.constant.domain.sprint.model.Sprint
 import esperer.constant.domain.sprint.persistence.entity.SprintJpaEntity
 import esperer.constant.global.mapper.GenericMapper
+import org.springframework.stereotype.Component
 
+@Component
 class SprintMapper : GenericMapper<Sprint, SprintJpaEntity>{
 
     override fun toDomain(entity: SprintJpaEntity?): Sprint? {
