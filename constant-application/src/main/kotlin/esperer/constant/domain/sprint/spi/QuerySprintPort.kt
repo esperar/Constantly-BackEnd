5767698761edff3888170ca449955e5ba03887cd
aesperer <piyrw9754@gmail.com>
2023-06-12 15:00:04 +0900

    create :: sprint persistence adapter

--- constant-application/src/main/kotlin/esperer/constant/domain/sprint/spi/QuerySprintPort.kt
+++ constant-application/src/main/kotlin/esperer/constant/domain/sprint/spi/QuerySprintPort.kt
@@ -1,8 +1,9 @@
 package esperer.constant.domain.sprint.spi
 
+import esperer.constant.domain.sprint.model.Sprint
 import java.util.UUID
 
 interface QuerySprintPort {
 
-    fun querySprintById(id: UUID)
+    fun querySprintById(id: UUID): Sprint?
 }
---
+++ constant-infrastructure/src/main/kotlin/esperer/constant/domain/sprint/persistence/SprintPersistenceAdapter.kt
@@ -0,0 +1,24 @@
+package esperer.constant.domain.sprint.persistence
+
+import esperer.constant.domain.sprint.mapper.SprintMapper
+import esperer.constant.domain.sprint.model.Sprint
+import esperer.constant.domain.sprint.persistence.repository.SprintRepository
+import esperer.constant.domain.sprint.spi.SprintPort
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class SprintPersistenceAdapter(
+    private val sprintMapper: SprintMapper,
+    private val sprintRepository: SprintRepository
+) : SprintPort {
+
+
+    override fun querySprintById(id: UUID): Sprint? =
+        sprintMapper.toDomain(sprintRepository.findByIdOrNull(id))
+
+
+    override fun save(sprint: Sprint): Sprint =
+        sprintMapper.toDomain(sprintRepository.save(sprintMapper.toEntity(sprint)))!!
+}
