cf0526e2d378dd0b4f7c4ddcb076e2ef84fab470
aesperer <piyrw9754@gmail.com>
2023-06-12 15:33:26 +0900

    create :: create sprint api

--- constant-infrastructure/src/main/kotlin/esperer/constant/domain/sprint/presentation/SprintWebAdapter.kt
+++ constant-infrastructure/src/main/kotlin/esperer/constant/domain/sprint/presentation/SprintWebAdapter.kt
@@ -1,6 +1,8 @@
 package esperer.constant.domain.sprint.presentation
 
+import esperer.constant.domain.sprint.dto.request.CreateSprintRequest
 import esperer.constant.domain.sprint.presentation.dto.request.CreateSprintWebRequest
+import esperer.constant.domain.sprint.usecase.CreateSprintUseCase
 import jakarta.validation.Valid
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
@@ -11,10 +13,19 @@ import org.springframework.web.bind.annotation.RestController
 @Validated
 @RestController
 @RequestMapping("/sprint")
-class SprintWebAdapter {
+class SprintWebAdapter(
+    private val createSprintUseCase: CreateSprintUseCase
+) {
 
     @PostMapping
     fun createSprint(@RequestBody @Valid request: CreateSprintWebRequest){
-
+        createSprintUseCase.execute(
+            CreateSprintRequest(
+                title = request.title,
+                target = request.target,
+                startDate = request.startDate,
+                endDateTime = request.endDate
+            )
+        )
     }
 }
