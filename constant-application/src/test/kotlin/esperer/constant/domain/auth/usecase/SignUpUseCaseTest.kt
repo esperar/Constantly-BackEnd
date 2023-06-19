12c77a1fb2e17acc091a40f1928877134a7950a5
aesperer <piyrw9754@gmail.com>
2023-06-19 19:52:59 +0900

    refactor :: signup도 token 반환

--- constant-application/src/test/kotlin/esperer/constant/domain/auth/usecase/SignUpUseCaseTest.kt
+++ constant-application/src/test/kotlin/esperer/constant/domain/auth/usecase/SignUpUseCaseTest.kt
@@ -1,6 +1,7 @@
 package esperer.constant.domain.auth.usecase
 
 import esperer.constant.common.service.SecurityService
+import esperer.constant.domain.auth.dto.request.SignUpRequest
 import esperer.constant.domain.auth.model.Authority
 import esperer.constant.domain.user.model.User
 import esperer.constant.domain.user.service.UserService
@@ -24,7 +25,6 @@ internal class SignUpUseCaseTest {
     private lateinit var signUpUseCase: SignUpUseCase
 
     private val email = "test1@gmail.com"
-    private val password = "encoded_test_password"
     private val name = "esperer"
     private val profileFileName = "profileFileName"
 
@@ -32,11 +32,20 @@ internal class SignUpUseCaseTest {
         User(
             id = UUID.randomUUID(),
             email = email,
-            password = password,
+            password = "encoded_test_password",
             name = name,
             profileFileName = profileFileName,
             authority = Authority.ROLE_USER,
             sprintId = null
+        )
+    }
+
+    private val requestStub: SignUpRequest by lazy {
+        SignUpRequest(
+            email = email,
+            password = "test_password",
+            name = name,
+            profileFileName = profileFileName
         )
     }
 
@@ -54,16 +63,21 @@ internal class SignUpUseCaseTest {
         val userStub = User(
             id = UUID.randomUUID(),
             email = email,
-            password = password,
+            password = "encoded_test_password",
             name = name,
             profileFileName = profileFileName,
             authority = Authority.ROLE_USER,
             sprintId = null
         )
 
+        given(userService.existsByEmail(requestStub.email)).willReturn(false)
+
+        given(securityService.encodePassword(requestStub.password)).willReturn(userStub.password)
+
+        given(userService.save(userStub)).willReturn(userStub)
 
         // then
-
+        signUpUseCase.execute(requestStub)
 
         // when
     }
